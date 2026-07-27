1fe1a9e8d4b791a717f96fcf09b38d41d0ae68e2
Pape Ibrahima KA <kaibrahima505@gmail.com>
2026-07-27 17:27:17 +0000

    JS : filtrage dynamique des tableaux, des intervenants et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -180,4 +180,217 @@ document.addEventListener("DOMContentLoaded", () =>{
     });
 
 
+    // ========== Affichage du planning par jour ==========
+    // selection des éléments
+    const btn1 = document.querySelector("#jr1");
+    const btn2 = document.querySelector("#jr2");
+    const btn3 = document.querySelector("#jr3");
+
+    const tab1 = document.querySelector("#jour1");
+    const tab2 = document.querySelector("#jour2");
+    const tab3 = document.querySelector("#jour3");
+    // Button1
+    if (btn1){      // if permet de vérifier d'abord avant d'exécuter
+            btn1.addEventListener("click", () =>{
+            tab1.style.display = "block";
+            tab2.style.display = "none";
+            tab3.style.display = "none";
+        });
+    }
+
+    // Button2
+    if (btn2){
+            btn2.addEventListener("click", () =>{
+            tab1.style.display = "none";
+            tab2.style.display = "block";
+            tab3.style.display = "none"; 
+        });
+    }
+    
+    // Button3
+    if (btn3){
+            btn3.addEventListener("click", () =>{
+            tab1.style.display = "none";
+            tab2.style.display = "none";
+            tab3.style.display = "block"; 
+        });
+    }
+    
+
+
+    //========= filtrage dynamique des freelances ==========
+    const btnTous = document.querySelector("#btn");
+    const btnIA = document.querySelector("#btnIA");
+    const btnBusi = document.querySelector("#btnBusiness");
+    const btnDesign = document.querySelector("#btnDesign");
+    const btnData = document.querySelector("#btnData");
+
+    const carteIA = document.querySelectorAll(".IA");
+    const carteBusiness = document.querySelectorAll(".business");
+    const carteDesign = document.querySelectorAll(".design");
+    const carteData = document.querySelectorAll(".data");
+
+    // Bouton Tous
+    if(btnTous){    // if permet de vérifier d'abord avant d'exécuter
+        btnTous.addEventListener("click", () =>{
+            carteIA.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteBusiness.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteDesign.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteData.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+        });
+    }
+    
+    // Bouton IA & Tech
+    if(btnIA){
+        btnIA.addEventListener("click", () =>{
+            carteIA.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteBusiness.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteDesign.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteData.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+        });
+    }
+    
+    // Bouton Business
+    if(btnBusi){
+        btnBusi.addEventListener("click", () =>{
+            carteIA.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteBusiness.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteDesign.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteData.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+        });
+    }
+    
+    // Bouton Design
+    if(btnDesign){
+        btnDesign.addEventListener("click", () =>{
+            carteIA.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteBusiness.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteDesign.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+            carteData.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+        });
+    }
+    
+    // Bouton Data
+    if(btnData){
+        btnData.addEventListener("click", () =>{
+            carteIA.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteBusiness.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteDesign.forEach((carte) =>{
+                carte.style.display = "none";
+            });
+            carteData.forEach((carte) =>{
+                carte.style.display = "block";
+            });
+        });
+    }
+    
+
+    //========== Validation du formulaire ===========
+    const form = document.getElementById('inscriptionForm');
+    const nomPrenom = document.getElementById('nom');
+    const email = document.getElementById('mail');
+    const tel = document.getElementById('telephone');
+    const pays = document.getElementById('pays');
+    const message = document.getElementById('message');
+    const confirmation = document.getElementById('confirmation');
+
+    if (form) {                     // if, permet de verifier d'abord avant d'executer
+        form.addEventListener('submit', function(event) {       // event, parametre permettant de stocker les infos declanchees par l'evenement submit 
+            let valid = true;
+            // Réinitialiser les messages d'erreur
+            document.getElementById("nomPrenomErreur").textContent = '';
+            document.getElementById("mailErreur").textContent = '';
+            document.getElementById("telErreur").textContent = '';
+            document.getElementById("paysErreur").textContent = '';
+            document.getElementById("messageErreur").textContent = '';
+
+            // Validation du nom
+            if (nomPrenom.value.trim() === "") {             // .trim(), permet de supprimer les espaces du debut et du fin
+                document.getElementById("nomPrenomErreur").textContent = 'Le nom et le prenom sont requis.';
+                document.getElementById("nom").style.borderColor = 'red';
+                valid = false;
+            }
+
+            // Validation de l'email
+            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!regex.test(mail.value)) {                  //.test(), permet de verifier si la valeur du champ est respecte
+                document.getElementById('mailErreur').textContent = 'Email invalide.';
+                document.getElementById("mail").style.borderColor = 'red';
+                valid = false;
+            }
+
+            if (tel.value.length < 8) {
+                document.getElementById("telErreur").textContent = 'Le numéro doit contenir au moins 8 chiffres.';
+                document.getElementById("telephone").style.borderColor = 'red';
+                valid = false;
+            }
+
+            // Validation du sujet
+            if (pays.value === 'choix') {           
+                document.getElementById('paysErreur').textContent = 'Le pays est requis.';
+                valid = false;
+            }
+
+            // Validation du message
+            if (message.value.length < 20) {
+                document.getElementById("messageErreur").textContent = 'Le message doit contenir au moins 20 caractères.';
+                document.getElementById("message").style.borderColor = 'red';
+                valid = false;
+            }
+
+            if (!valid) {               // Si le formulaire n’est PAS valide
+                event.preventDefault(); // Empêche la soumission du formulaire
+            } else {
+                // Afficher un message de succès
+                
+                // Empêche la soumission immédiate
+                event.preventDefault();
+
+                // Affiche le message
+                confirmation.textContent = "Message envoyé avec succès !";
+
+                // Attend 0.5 secondes puis soumet le formulaire
+                setTimeout(() => {
+                    form.submit();
+                }, 500);
+            }
+        });
+    };
+    
 })
